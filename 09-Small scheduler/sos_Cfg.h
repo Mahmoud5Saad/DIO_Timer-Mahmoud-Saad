92e5b24817d36c7635f8943d0b3b9b15e0286b75
Mahmoud Saad <mahmoudmsaad2@gmail.com>
2020-03-02 18:40:54 +0200

    Add AUTOSAR and Small OS projects

---
+++ 09-Small scheduler/sos_Cfg.h
@@ -0,0 +1,16 @@
+/*
+ * sos_Cfg.h
+ *
+ * Created: Monday, 20-Jan-20 1:32:44 PM
+ *  Author: Mahmoud Saad
+ */ 
+
+
+#ifndef SOS_CFG_H_
+#define SOS_CFG_H_
+
+#define SOS_MAX_NUMBER_OF_TASKS		(5U)
+
+
+
+#endif /* SOS_CFG_H_ */
